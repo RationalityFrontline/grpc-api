2ad0cc03992b55f509d46c72459748fb095916ef
RationalityFrontline <rationalityfrontline@gmail.com>
2020-10-14 23:27:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# grpc-api  [![JCenter Version](https://img.shields.io/bintray/v/rationalityfrontline/grpc/grpc-api?label=JCenter)](https://bintray.com/rationalityfrontline/grpc/grpc-api)  [![Apache License 2.0](https://img.shields.io/github/license/rationalityfrontline/grpc-api)](https://github.com/RationalityFrontline/grpc-api/blob/master/LICENSE)
+
+This library is a temporary workaround for the issue ["gRPC Java is not usable from Java 9 modules"](https://github.com/grpc/grpc-java/issues/3522). 
+It solves the split package problem mentioned in the issue by merging `grpc-api` and `grpc-context` into a single jar.
+
+## Usage
+
+Just add the following code blocks to your build.gradle.kts file.
+
+**Gradle Kotlin DSL:**
+
+```groovy
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation("org.rationalityfrontline.grpc:grpc-api:1.32.1")
+}
+
+configurations.all {
+    exclude(group = "io.grpc", module = "grpc-context")
+    exclude(group = "io.grpc", module = "grpc-api")
+    //uncomment the following line if you have "javax.annotation:javax.annotation-api" in your dependencies.
+    //exclude(group = "com.google.code.findbugs", module = "jsr305")
+}
+```
+
+## Reference
+
+https://github.com/grpc/grpc-java/issues/3522
+
+## License
+
+grpc-api is released under the [Apache 2.0 license](https://github.com/RationalityFrontline/grpc-api/blob/master/LICENSE).
+
+```
+Copyright 2020 RationalityFrontline
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
---
+++ build.gradle.kts
@@ -0,0 +1,76 @@
+import com.jfrog.bintray.gradle.BintrayExtension.PackageConfig
+import com.jfrog.bintray.gradle.BintrayExtension.VersionConfig
+import java.util.Date
+
+plugins {
+    `maven-publish`
+    id("com.jfrog.bintray") version "1.8.5"
+}
+
+group = "org.rationalityfrontline.grpc"
+version = "1.32.1"
+
+val NAME = project.name
+val DESC = "Merged grpc-api and grpc-context into a single jar"
+val GITHUB_REPO = "RationalityFrontline/grpc-api"
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenPublish") {
+
+            artifact(file("libs/grpc-api-1.32.1.jar"))
+            pom {
+                name.set(NAME)
+                description.set("$NAME $version - $DESC")
+                url.set("https://github.com/$GITHUB_REPO")
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("RationalityFrontline")
+                        email.set("rationalityfrontline@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/$GITHUB_REPO")
+                }
+            }
+        }
+    }
+}
+
+bintray {
+    fun env(propertyName: String): String {
+        return if (project.hasProperty(propertyName)) {
+            project.property(propertyName) as String
+        } else "Unknown"
+    }
+
+    user = env("BINTRAY_USER")
+    key = env("BINTRAY_KEY")
+    publish = true
+    override = true
+    setPublications("mavenPublish")
+    pkg(closureOf<PackageConfig>{
+        repo = "grpc"
+        name = NAME
+        desc = DESC
+        setLabels("grpc", "grpc-api", "grpc-context", "jpms")
+        setLicenses("Apache-2.0")
+        publicDownloadNumbers = true
+        githubRepo = GITHUB_REPO
+        vcsUrl = "https://github.com/$githubRepo"
+        websiteUrl = vcsUrl
+        issueTrackerUrl = "$vcsUrl/issues"
+        version(closureOf<VersionConfig> {
+            name = "${project.version}"
+            desc = DESC
+            released = "${Date()}"
+            vcsTag = name
+        })
+    })
+}
